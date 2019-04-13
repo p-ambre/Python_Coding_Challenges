2c143509e860effb715f4ae9b0457650884731b5
p-ambre <pradnya.ambre16@gmail.com>
2019-04-12 22:37:33 -0400

    implementing binary search and returning the insert position of an element

--- 35_Leetcode_SearchInsertPosition.py
+++ 35_Leetcode_SearchInsertPosition.py
@@ -23,15 +23,16 @@ Output: 0
 
 """
 
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        if target in nums:
-            return(nums.index(target))
-        else:
-            if target < nums[0]:
-                return 0
-            for i in range(n-1):
-                if nums[i] < target < nums[i+1]:
-                    return(i+1)
-            return(n)
+        l,r = 0,len(nums)-1
+        while (l <= r):
+            mid = round((l+r)/2)
+            if (nums[mid] < target):
+                l = mid + 1
+            elif (nums[mid] > target):
+                r = mid - 1
+            else:
+                return mid
+        return l
