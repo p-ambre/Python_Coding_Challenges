6b38a54007bfc986c9ae810c313ca8309a7bf52e
p-ambre <pradnya.ambre16@gmail.com>
2019-04-16 13:55:58 -0400

    check if the given string can be formed by its substring

--- 459_Leetcode_RepeatedSubstringPattern.py
+++ 459_Leetcode_RepeatedSubstringPattern.py
@@ -5,35 +5,54 @@ it and appending multiple copies ofthe substring together. You may assume the gi
 string consists of lowercase English letters only and its length will not exceed 10000.
 
 Example 1:
-Input: "abab"
+Input: "abab"  n = 4 pattern = "ab"
 Output: True
 Explanation: It's the substring "ab" twice.
 
 Example 2:
-Input: "aba"
+Input: "aba" n = 3 pattern = ""
 Output: False
 
 Example 3:
-Input: "abcabcabcabc"
+Input: "abcabcabcabc" n = 12 pattern = "abc" or "abcabc"
 Output: True
-Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+Example 4:
+Input: "abcabcabc" n = 9 pattern = "abc"
+Output: True
+
+Example 5:
+Input: "abcd" n = 4 pattern = ""
+Output: False
+
+Example 6:
+Input: "babbabbabbabbab" n = 13 pattern = "bab"
+Output: True
+
+Explanation:
+n can be even or odd
+i starts from 1 because in pattern we need to have at least one element
 
 """
 
 class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
+    def repeatedSubstringPattern(self, s):
         n = len(s)
-        for i in range(1, (n//2)+1):
-            if (n % i) == 0:
-                count = n//i
-                pattern = s[0:i]
-                if count*pattern == s:
-                    return True
+        else:
+            for i in range(1, (n//2)+1):
+                res = ""
+                if (n % i) == 0:
+                    count = n//i
+                    pattern = s[0:i]
+                    for j in range(count):
+                        res += pattern
+                    if res == s:
+                        return True
+                    else:
+                        continue
                 else:
                     continue
-            else:
-                continue
-        return False
+            return False
 
 s = str(input())
 p1 = Solution()
